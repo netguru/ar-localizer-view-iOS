4d1b0dbbd5e836b390c233051d15c527f44ce6a0
jedrzej.gronek <jedrzej.gronek@netguru.com>
2019-10-21 10:12:36 +0200

    Create XCode project

---
+++ AR Localizer View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AR Localizer View
+//
+//  Created by Jędrzej Gronek on 21/10/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

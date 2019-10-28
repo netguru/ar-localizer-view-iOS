77b18c9525ee1aa399ed58a97592cee339fb8248
jedrzej.gronek <jedrzej.gronek@netguru.com>
2019-10-28 18:29:01 +0100

    Update AppDelegate to load ARViewController

--- AR Localizer/Source Files/AppDelegate.swift
+++ AR Localizer/Source Files/AppDelegate.swift
@@ -7,5 +7,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
 
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window!.rootViewController = ARViewController()
+    window!.makeKeyAndVisible()
+
+    return true
+  }
 }

bfb6bd21d9cc16184c6a547d41373e6f9f9fc53e
jedrzej.gronek <jedrzej.gronek@netguru.com>
2019-11-20 19:07:40 +0100

    Add POITests

---
+++ Unit Tests/Source Files/Models/POITests.swift
@@ -0,0 +1,32 @@
+//
+//  POITests.swift
+//  ARLocalizerView
+//
+
+import XCTest
+import CoreLocation
+@testable import ARLocalizerView
+
+let poi = POI(name: "netguru-HQ", latitude: 52.4015279, longitude: 16.8918892)
+
+class POITests: XCTestCase {
+    func testInit() {
+        XCTAssertEqual(poi.name, "netguru-HQ")
+        XCTAssertEqual(poi.latitude, 52.4015279)
+        XCTAssertEqual(poi.longitude, 16.8918892)
+    }
+
+    func testCLLocation() {
+        let clLocation = CLLocation(latitude: 52.4015279, longitude: 16.8918892)
+        XCTAssertEqual(clLocation.distance(from: poi.clLocation), 0)
+    }
+
+    func testMKAnnotationTitle() {
+        XCTAssertEqual(poi.title, "netguru-HQ")
+    }
+
+    func testMKAnnotationCoordiante() {
+        XCTAssertEqual(poi.coordinate.latitude, 52.4015279)
+        XCTAssertEqual(poi.coordinate.longitude, 16.8918892)
+    }
+}
